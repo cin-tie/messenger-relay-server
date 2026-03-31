56281ea233215687416b4d5847f05a9cb163c55f
cin-tie <csizegod@gmail.com>
2026-03-31 13:41:30 +0300

    Server writed

--- src/main/java/com/cintie/messenger/network/RelayServer.java
+++ src/main/java/com/cintie/messenger/network/RelayServer.java
@@ -1,4 +1,28 @@
 package com.cintie.messenger.network;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class RelayServer {
+    public final int port;
+    public final ConnectionRegistry connectionRegistry = new ConnectionRegistry();
+
+    public RelayServer(int port){
+        this.port = port;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Relay running on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                ClientHandler handler = new ClientHandler(socket, connectionRegistry);
+                new Thread(handler).start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
