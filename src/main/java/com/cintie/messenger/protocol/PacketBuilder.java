77637c1e73b77520e3241fc92b9afd4e707969b7
cin-tie <csizegod@gmail.com>
2026-05-22 08:46:20 +0300

    Hello creation changed

--- src/main/java/com/cintie/messenger/protocol/PacketBuilder.java
+++ src/main/java/com/cintie/messenger/protocol/PacketBuilder.java
@@ -35,11 +35,11 @@ public class PacketBuilder {
     }
 
     // Create hello message
-    public static Packet hello(String senderId) {
+    public static Packet hello(String recipientId) {
         return base(
                 PacketType.HELLO,
-                senderId,
-                null,
+                "Server",
+                recipientId,
                 null
         );
     }
